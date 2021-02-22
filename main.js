cdb9314f3e058bb972de2c9e9f9e109824e9352e
christinagaglio <31930238+christinagaglio@users.noreply.github.com>
2021-02-22 11:54:08 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,23 +3,71 @@ let currentCatTax = 0;
 
 // TODO: calcButtonClick function
 // Function should handle the following items:
-// 1) Generate a random, whole number between 0 and 20.
-// 2) If the random number is not 0, update the amountOwed div to display "You owe {random number} cat tax! Pay up!"
-// 3) If the random number is not 0, update the pay button text to display "Pay Cat Tax"
-// 4) If the random number is not 0, update the pay button so that it is no longer hidden
-// 5) If the random number is 0, update the amountOwed div to display "You owe {random number} cat tax! You've escaped this time!"
-// 6) If the random number is 0, update the pay button so that it is hidden.
-// 7) Both the amountOwed and pay amount button should be updated every time the calculate cat tax button is clicked.
 
-function calcButtonClick() {}
+function calcButtonClick(){
+    var x = document.getElementById("amountOwed");
 
+    // 1) Generate a random, whole number between 0 and 20.
+    var random = Math.floor(Math.random * 20);
+
+    // 2) If the random number is not 0, update the amountOwed div to display "You owe {random number} cat tax! Pay up!"
+    if (random != 0) {
+        x.display("You owe" + random; "cat tax! Pay up now!");
+    }
+
+    // 3) If the random number is not 0, update the pay button text to display "Pay Cat Tax"
+    if (random != 0){
+        x.display("Pay Cat Tax")
+    }
+
+    // 4) If the random number is not 0, update the pay button so that it is no longer hidden
+    if (random !=0){
+        x.style.display = "none";
+    }
+
+    // 5) If the random number is 0, update the amountOwed div to display "You owe {random number} cat tax! You've escaped this time!"
+    if (random ==0){
+        x.display("You owe " + random; "cat tax! You've escaped this time!")
+    }
+
+    // 6) If the random number is 0, update the pay button so that it is hidden.
+    if (random == 0){
+        x.style.display = "block";
+    }
+
+    // 7) Both the amountOwed and pay amount button should be updated every time the calculate cat tax button is clicked.
+    if (button.isclicked()){
+        
+    }
+
+}//calcButtonClick
+
+
+function payButton() {
 // TODO: payButton function
 // Function should handle the following items:
+    var x = document.getElementById("amountOwed");
 // 1) Decrement the currentCatTax amount by 1
-// 2) If the remaining cat tax is greater than zero, update the amountOwed div to display "You still owe {remaining amount} cat tax! Pay up!"
-// 3) If the remaining cat tax is zero or fewer, update the amountOwed div to display "Your debts are paid..."
-// 4) If the cat tax was payable (amount was greater than 0) when the button was clicked, make a call to the cat api to get a cat image (https://api.thecatapi.com/v1/images/search)
-// 5) Once the cat image is retrieved, append the image to the image container
-// 6) If the cat wax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
+    currentCatTax = currentCatTax - 1; 
 
-function payButton() {}
+// 2) If the remaining cat tax is greater than zero, update the amountOwed div to display "You still owe {remaining amount} cat tax! Pay up!"
+    if (currentCatTax > 0) {
+        x.display("You still owe" + currentCatTax; "cat tax! Pay up!")
+    }
+
+// 3) If the remaining cat tax is zero or fewer, update the amountOwed div to display "Your debts are paid..."
+    if (currentCatTax <= 0 ){
+        x.display("Your debts are paid")
+    }
+
+// 4) If the cat tax was payable (amount was greater than 0) when the button was clicked, make a call to the cat api to get a cat image (https://api.thecatapi.com/v1/images/search)
+    if (currentCatTax > 0 ){
+
+    }
+
+// 5) Once the cat image is retrieved, append the image to the image container
+
+
+// 6) If the cat tax was not payable (amount was less than or equal to 0) when the button was clicked, replace the entire contents of the container with the gif found here (https://gfycat.com/snivelingbeautifuljoey-cat)
+
+}
